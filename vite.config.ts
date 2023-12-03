e49afb79c4c96d57d73879e80b40a7d3d5ed232c
menyadeniskostin <menyadeniskostin@gmail.com>
2023-12-03 19:25:29 +0300

    Vite&Tauri configured for multi-window development

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
+import path, { resolve } from "path";
+
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+    root: root,
     plugins: [vue()],
 
     // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
@@ -12,7 +16,7 @@ export default defineConfig(async () => ({
     clearScreen: false,
     // 2. tauri expects a fixed port, fail if that port is not available
     server: {
-        port: 1420,
+        port: 5173,
         strictPort: true,
     },
     // 3. to make use of `TAURI_DEBUG` and other env variables
@@ -30,5 +34,18 @@ export default defineConfig(async () => ({
             "@": path.resolve(__dirname, "src"),
         },
     },
+    build: {
+        outDir: outDir,
+        emptyOutDir: true,
+        rollupOptions: {
+            // Needed for multiple windows (main, vaults, etc.)
+            input: {
+                main: resolve(root, "index.html"),
+                /* vite docs:
+                During dev, simply navigate or link to /vaults/ - it works as 
+                expected, just like for a normal static file server. */
+                vaults: resolve(root, "windows/vaults/index.html"),
+            },
+        },
+    },
 }));
-
